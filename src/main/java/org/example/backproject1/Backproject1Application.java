fa12ba67c43eb083a8b2b394ec94a0a3fbe17a45
sarapoba <lhk981112@gmail.com>
2026-02-24 12:17:44 +0900

    백엔드 초기설정

---
+++ src/main/java/org/example/backproject1/Backproject1Application.java
@@ -0,0 +1,13 @@
+package org.example.backproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backproject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backproject1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/backproject1/Backproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backproject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
